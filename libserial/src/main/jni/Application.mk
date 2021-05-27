6f00ade14608fcba90c4bdf72d0d25dc16864d72
Michel Martinez <michelsm1990@gmail.com>
2021-05-27 14:53:52 -0300

    fix: change sdk min to 19

--- libserial/src/main/jni/Application.mk
+++ libserial/src/main/jni/Application.mk
@@ -6,7 +6,7 @@ else
 APP_OPTIM := release
 endif
 
-APP_PLATFORM := android-28
+APP_PLATFORM := android-19
 APP_ABI := all
 APP_STL := c++_static
 #APP_CFLAGS := -std=gnu11
--- libserial/src/main/jni/libs/serialport/list_ports_linux.cc
+++ libserial/src/main/jni/libs/serialport/list_ports_linux.cc
@@ -54,21 +54,21 @@ glob(const vector<string>& patterns)
 
     glob_t glob_results;
 
-    int glob_retval = glob(patterns[0].c_str(), 0, NULL, &glob_results);
+    int glob_retval = 0; //glob(patterns[0].c_str(), 0, NULL, &glob_results);
 
     vector<string>::const_iterator iter = patterns.begin();
 
-    while(++iter != patterns.end())
-    {
-        glob_retval = glob(iter->c_str(), GLOB_APPEND, NULL, &glob_results);
-    }
+    // while(++iter != patterns.end())
+    // {
+    //    glob_retval = glob(iter->c_str(), GLOB_APPEND, NULL, &glob_results);
+    // }
 
     for(int path_index = 0; path_index < glob_results.gl_pathc; path_index++)
     {
         paths_found.push_back(glob_results.gl_pathv[path_index]);
     }
 
-    globfree(&glob_results);
+    // globfree(&glob_results);
 
     return paths_found;
 }
